5c62c98ce64c39acc9efd512221f047ac9f29896
nupurmithila44 <155286099+nupurmithila44@users.noreply.github.com>
2024-01-28 21:23:02 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+const numbers = [ 21, 20, 34, 54, 55, 64, 78];
+
+// console.log(numbers.length);
+// console.log(numbers);
+// console.log(numbers[3]);
+
+numbers[2] = 999;
+
+console.log(numbers);
